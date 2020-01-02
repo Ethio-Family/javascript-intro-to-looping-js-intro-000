ef5b76de35c529b1bf336b6123d411f73e688380
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-02 05:15:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -30,7 +30,7 @@ function whileLoop(number) {
   return 'done';
 }
 
-let incrementVariable();
+var incrementVariable();
 function doWhileLoop(number) {
   do {
   console.log("I run once regardless.");
